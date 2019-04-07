52bb5b3ffcc3fefd493adf433a94c0d5a499807d
furuhama <furuhama@users.noreply.github.com>
2019-04-07 12:07:27 +0900

    Update strcat.c

--- src/strcat.c
+++ src/strcat.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
+    // To concatenate two string literals, just write them sequencially.
     char str[] = "HOGE" "FUGA";
     printf("%s\n", str);
+
+    // To concatenate two string variables, use `strcat` function.
+    char str1[10] = "HOGE";
+    char str2[] = "FUGA";
+    strcat(str1, str2);
+    printf("%s\n", str1);
 
     return 0;
 }
