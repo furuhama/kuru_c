7377dadb52f7b0ddb713806725dd0df822bf6695
furuhama <furuhama@users.noreply.github.com>
2019-04-14 16:57:41 +0900

    Update

--- src/pointer_val.c
+++ src/pointer_val.c
@@ -3,18 +3,15 @@
 int main() {
     int *p, i;
     p = &i;
-
     printf("p = %p\n", p);
     printf("&i = %p\n", &i);
-
     // Outputs are like this
     // p = 0x7ffee4c3e934
     // &i = 0x7ffee4c3e934
 
+    // Init pointer variable with null
     int *np = NULL;
-
     printf("np = %p\n", np);
-
     // np = 0x0
 
 
