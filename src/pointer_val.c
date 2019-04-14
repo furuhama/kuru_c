bae91358084958872cfb67adf4f91fff89bcd120
furuhama <furuhama@users.noreply.github.com>
2019-04-14 16:54:41 +0900

    pointer_val.c

---
+++ src/pointer_val.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int *p, i;
+    p = &i;
+
+    printf("p = %p\n", p);
+    printf("&i = %p\n", &i);
+
+    // Outputs are like this
+    // p = 0x7ffee4c3e934
+    // &i = 0x7ffee4c3e934
+
+    return 0;
+}
